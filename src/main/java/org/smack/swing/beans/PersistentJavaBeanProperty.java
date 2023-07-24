48a4552fac65480a12fdf1229f75f63632f391ed
Michael Binz <michab66@gmail.com>
2023-07-24 22:36:58 +0200

    Fixes #28

--- src/main/java/org/smack/swing/beans/PersistentJavaBeanProperty.java
+++ src/main/java/org/smack/swing/beans/PersistentJavaBeanProperty.java
@@ -1,25 +1,25 @@
-/* $Id$
+/*
+ * smack_swing @ https://github.com/smacklib/dev_smack_swing
  *
- * Unpublished work.
- * Copyright © 2016 Michael G. Binz
+ * Copyright © 2016-2023 Michael G. Binz
  */
 package org.smack.swing.beans;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.smack.swing.application.Application;
 import org.smack.swing.application.ApplicationProperties;
+import org.smack.util.JavaUtil;
 import org.smack.util.ServiceManager;
+import org.smack.util.StringUtil;
 import org.smack.util.converters.StringConverter;
 
 /**
- * A JavaBean property that uses ApplicationProperties for persistence.
+ * A JavaBean property that uses {@link ApplicationProperties} for persistence.
  *
  * @param <T> The property's type.
  * @param <B> The bean the property is part of.
  *
- * @version $Rev: 1521 $
  * @author Michael G. Binz
  */
 public class PersistentJavaBeanProperty<T,B> extends JavaBeanProperty<T,B>
@@ -27,17 +27,21 @@ public class PersistentJavaBeanProperty<T,B> extends JavaBeanProperty<T,B>
     private static final Logger LOG =
             Logger.getLogger( PersistentJavaBeanProperty.class.getName() );
 
-    private Application _app;
-
     private final StringConverter _converter;
 
+    private final String _keyName;
+
     /**
-     * Create an instance.
+     * Create an instance.  The passed bean has to offer a JavaBean
+     * property for 'name' that must be set.  This name is used in
+     * the persistence store to match a bean against its value.
      *
      * @param bean A reference to our bean.
      * @param initialValue The property's initial value.
      * @param propertyName The name of the property.
      * @param converter A string converter for the property type.
+     *
+     * @throws Exception if bean does not offer a 'name' property.
      */
     public PersistentJavaBeanProperty(
             B bean,
@@ -47,33 +51,23 @@ public class PersistentJavaBeanProperty<T,B> extends JavaBeanProperty<T,B>
     {
         super( bean, initialValue, propertyName );
 
-        try
-        {
-            // If the passed object is an application, then this
-            // means we are an application property defined on the
-            // application object. In this case the .getInstance
-            // call is not working. By using the passed object
-            // we allow persistent java bean properties defined
-            // on the application object using inline initialization.
-            // This is cool, cool, cool...
-            _app = (Application)bean;
-        }
-        catch ( ClassCastException e )
-        {
-            _app = Application.getInstance();
-        }
+        PropertyProxy<String, B> named = new PropertyProxy<>( "name", bean );
+        JavaUtil.Assert( String.class.equals( named.getType() ) );
+        String name = named.get();
+        JavaUtil.Assert( StringUtil.hasContent( name ), "Empty name." );
+
+        _keyName = name + ":" + propertyName;
 
         _converter = converter;
 
         String value =
-                getAps().get( bean.getClass(), propertyName, null );
+                getAps().get( bean.getClass(), _keyName, null );
 
         if ( value != null )
         {
             try
             {
                 super.set( _converter.convert( getType(), value ) );
-                // All worked, object is initialized, we're done.
                 return;
             }
             catch ( Exception e )
@@ -93,11 +87,20 @@ public class PersistentJavaBeanProperty<T,B> extends JavaBeanProperty<T,B>
     @Override
     public void set( T newValue )
     {
+        // TODO: we need a conversion from T->String, but the
+        // StringConverter currently only supports String->T.
+        // For now we fall back to #toString() but this may fail
+        // for complex types.
+        String value =
+                newValue == null ?
+                        StringUtil.EMPTY_STRING :
+                        newValue.toString();
+
         // Put the value into the persistence.
         getAps().put(
                 getBean().getClass(),
-                getName(),
-                "" ); // TODO _converter.toString( newValue ) );
+                _keyName,
+                value );
 
         super.set( newValue );
     }
